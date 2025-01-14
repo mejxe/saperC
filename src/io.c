b36f8639c258602856a0fac2f4d87128466415f5
mejxe <mlaskowski707@gmail.com>
2025-01-14 19:36:15 +0100

    Add input safety check, add different difficulties

--- src/io.c
+++ src/io.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include "io.h"
+#include <string.h>
 #define MAX_BUFF 20
+void clear_buff() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
 int get_difficulty() {
     int diff;
     while (1) {
-        printf("Wybierz poziom trudnosci:\n-Latwy (1)\n-Średni (2)\n-Trudny(3)");
-        if (scanf("%d", &diff) != 0 && diff <= 1 && diff <= 3) break;
+        printf("Wybierz poziom trudnosci:\n-Latwy (1)\n-Średni (2)\n-Trudny(3)\n");
+        if (scanf("%d", &diff) == 1 && diff >= 1 && diff <= 3) break;
     }
+    clear_buff();
     return diff;
 }
 void user_move(minefield* plansza) {
@@ -18,12 +24,11 @@ void user_move(minefield* plansza) {
         fgets(input, sizeof(input), stdin); // Odczyt danych wejściowych
 
         // Parsowanie danych wejściowych
-        if (sscanf(input, "%d %d %d", &x, &y, &f) == 3) {
+        if ((sscanf(input, "%d %d %d", &x, &y, &f) == 3) && x>=0 && x<plansza->x && y>=0 && y<plansza->y && f>=0 && f<2 ) {
             break;
-        } else {
-            printf("Błąd: Niepoprawny format danych wejściowych. Spróbuj ponownie.\n");
         }
-        printf("%d %d %d ",x,y,f);
+        printf("Błąd: Niepoprawny format danych wejściowych. Spróbuj ponownie.\n");
+        input[strcspn(input, "\n")] = '\0';
     }
     check_field(plansza, x, y);
 }
--- src/main.c
+++ src/main.c
@@ -4,7 +4,27 @@
     
 int main() {
     int x, y, f;
-    minefield* plansza = create_minefield(5, 5, 3);
+    minefield* plansza;
+    int difficulty = get_difficulty();
+    switch (difficulty) {
+        case 1: {
+        printf("Wybrano Łatwy\n");
+        plansza = create_minefield(9, 9, 10);
+        break;
+                }
+        case 2: {
+        printf("Wybrano Średni\n");
+        plansza = create_minefield(16, 16, 30);
+        break;
+                }
+        case 3: {
+        printf("Wybrano Trudny\n");
+        plansza = create_minefield(16, 30, 99);
+        break;
+                }
+    }
+    
+    
     display_minefield(plansza);
     user_move(plansza);
     display_minefield(plansza);
